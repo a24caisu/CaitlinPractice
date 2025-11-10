69f7c6949e7c93fba6c2a313165437173c9c6c11
a24caisu <a24caisu@student.his.se>
2025-11-10 17:10:29 +0100

    Empty file

---
+++ My project/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+    private Vector3 Pos;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
